f88325f0dce1b84db18e214d73d0e305661671ff
Spiros Makris <makris.spiros@gmail.com>
2019-11-16 12:59:22 +0200

    Initial player motion - @DONE 1.1

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
-rltk::add_wasm_support!();
-use rltk::{Console, GameState, Rltk, RGB};
+use rltk::{Console, GameState, Rltk, RGB, VirtualKeyCode};
 use specs::prelude::*;
-// use std::cmp::{max, min};
+use std::cmp::{max, min};
 #[macro_use]
 extern crate specs_derive;
 
-// Comonenents
+rltk::add_wasm_support!();
 
+// Components
 #[derive(Component)]
 struct Position {
     x: i32,
@@ -22,6 +22,9 @@ struct Renderable {
 
 #[derive(Component)]
 struct LeftMover {}
+
+#[derive(Component, Debug)]
+struct Player {}
 
 // SYSTEMS
 struct LeftWalker {}
@@ -56,14 +59,41 @@ impl State {
 impl GameState for State {
     fn tick(&mut self, ctx: &mut Rltk) {
         ctx.cls();
-        // ctx.print(1, 1, "Hello Rust World");
+
+        player_input(self, ctx);
 
         self.run_systems();
+
+        
         let positions = self.ecs.read_storage::<Position>();
         let renderables = self.ecs.read_storage::<Renderable>();
 
         for (pos, render) in (&positions, &renderables).join() {
             ctx.set(pos.x, pos.y, render.fg, render.bg, render.glyph);
+        }
+    }
+}
+
+fn try_move_player(delta_x: i32, delta_y: i32, ecs: &mut World) {
+    let mut positions = ecs.write_storage::<Position>();
+    let mut players = ecs.write_storage::<Player>();
+
+    for (_player, pos) in (&mut players, &mut positions).join() {
+        pos.x = min(79, max(0, pos.x + delta_x));
+        pos.y = min(49, max(0, pos.y + delta_y));
+    }
+}
+
+fn player_input(gs: &mut State, ctx: &mut Rltk) {
+    // Player movement
+    match ctx.key {
+        None => {}, // Nothing happened
+        Some(key) => match key {
+            VirtualKeyCode::Left => try_move_player(-1, 0, &mut gs.ecs),
+            VirtualKeyCode::Right => try_move_player(1, 0, &mut gs.ecs),
+            VirtualKeyCode::Up => try_move_player(0, -1, &mut gs.ecs),
+            VirtualKeyCode::Down => try_move_player(0, 1, &mut gs.ecs),
+            _ => {}
         }
     }
 }
@@ -78,8 +108,9 @@ fn main() {
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
     gs.ecs.register::<LeftMover>();
+    gs.ecs.register::<Player>();
 
-    // Create an entity
+    // Create a player entity
     gs.ecs
         .create_entity()
         .with(Position { x: 40, y: 25 })
@@ -88,6 +119,7 @@ fn main() {
             fg: RGB::named(rltk::YELLOW),
             bg: RGB::named(rltk::BLACK),
         })
+        .with(Player{})
         .build();
 
     // Create multiple entities
