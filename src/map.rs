d15080924d16a3143f6bcafe3c4be1d55010a299
Spiros Makris <makris.spiros@gmail.com>
2019-11-17 11:18:05 +0200

    Added a corridor

--- src/map.rs
+++ src/map.rs
@@ -1,5 +1,6 @@
 use rltk::{Rltk, RGB, Console};
 use super::{Rect};
+use std::cmp::{min, max};
 
 // Copy & Clone allow this enum to be used
 // as a `value` type, that is, passed around by value
@@ -78,6 +79,24 @@ fn apply_room_to_map(room: &Rect, map: &mut [TileType]) {
   }
 }
 
+fn apply_horizontal_tunnel(map: &mut [TileType], x1: i32, x2: i32, y: i32) {
+  for x in min(x1, x2) ..= max(x1, x2) {
+    let idx = xy_idx(x, y);
+    if idx > 0 && idx < 80*50 {
+      map[idx as usize] = TileType::Floor;
+    }
+  }
+}
+
+fn apply_vertical_tunnel(map: &mut [TileType], y1: i32, y2: i32, x: i32) {
+  for y in min(y1, y2) ..= max(y1, y2) {
+    let idx = xy_idx(x, y);
+    if idx > 0 && idx < 80*50 {
+      map[idx as usize] = TileType::Floor;
+    }
+  }
+}
+
 pub fn new_map_rooms_and_corridors() -> Vec<TileType> {
   let mut map = vec![TileType::Wall; 80*50];
 
@@ -86,6 +105,8 @@ pub fn new_map_rooms_and_corridors() -> Vec<TileType> {
 
   apply_room_to_map(&room1, &mut map);
   apply_room_to_map(&room2, &mut map);
+
+  apply_horizontal_tunnel(&mut map, 25, 40, 23);
 
   map
 }
